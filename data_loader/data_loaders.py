64597b9f1702f4fe2fc8051f74a05de79c04cd45
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2023-12-04 18:25:21 -0600

    data_loaders.BouncingMnistDataLoader: data loader for Bouncing MNIST
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -46,3 +46,15 @@ class MnistDataLoader(BaseDataLoader):
         self.data_dir = data_dir
         self.dataset = datasets.MNIST(self.data_dir, train=training, download=True, transform=trsfm)
         super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
+
+class BouncingMnistDataLoader(BaseDataLoader):
+    """
+    Bouncing MNIST data loading using BaseDataLoader
+    """
+    def __init__(self, data_dir, batch_size, shuffle=True, validation_split=0.0, num_workers=1, training=True):
+        trsfm = transforms.Compose([
+            transforms.ToTensor(),
+        ])
+        self.data_dir = data_dir
+        self.dataset = BouncingMNIST(self.data_dir, transform=trsfm)
+        super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
