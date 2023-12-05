bb74c60bb7d9d3398353b366d740ccdff2965bf8
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2023-12-04 18:26:21 -0600

    model.BouncingMnistModel: employ amortized SVI as a baseline guide
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- model/model.py
+++ model/model.py
@@ -1,8 +1,11 @@
 import pyro
 import pyro.distributions as dist
+import pyro.nn as pnn
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from base import BaseModel
+from .inference import asvi, mlp_amortizer
 
 class DigitPositions(BaseModel):
     def __init__(self, z_where_dim=2):
@@ -62,20 +65,63 @@ class DigitsDecoder(BaseModel):
         return pyro.sample("X_%d" % t, likelihood, obs=x)
 
 class BouncingMnistModel(BaseModel):
-    def __init__(self, digit_side=28, hidden_dim=400, num_classes=10,
-                 num_digits=3, x_side=96, z_what_dim=10, z_where_dim=2):
+    def __init__(self, digit_side=28, hidden_dim=400, num_digits=3, T=10,
+                 x_side=96, z_what_dim=10, z_where_dim=2):
         super().__init__()
-        self._num_classes = num_classes
         self._num_digits = num_digits
+
+        self.encoders = pnn.PyroModule[nn.ModuleDict]({
+            'mean_fields': pnn.PyroModule[nn.ModuleDict]({
+                'z_what': pnn.PyroModule[nn.ModuleDict]({
+                    'loc': mlp_amortizer(T * x_side ** 2, hidden_dim,
+                                         num_digits * z_what_dim),
+                    'scale': mlp_amortizer(T * x_side ** 2, hidden_dim,
+                                           num_digits * z_what_dim),
+                }),
+                'z_where': pnn.PyroModule[nn.ModuleDict]({
+                    'loc': mlp_amortizer(x_side ** 2, hidden_dim,
+                                         num_digits * z_where_dim),
+                    'scale': mlp_amortizer(x_side ** 2, hidden_dim,
+                                           num_digits * z_where_dim),
+                })
+            }),
+            'mixing_logits': pnn.PyroModule[nn.ModuleDict]({
+                'z_what':  mlp_amortizer(T * x_side ** 2,
+                                         num_digits * z_what_dim, 1),
+                'z_where':  mlp_amortizer(x_side ** 2, num_digits * z_where_dim,
+                                          1)
+            }),
+        })
 
         self.decoder = DigitsDecoder(digit_side, hidden_dim, x_side, z_what_dim)
         self.digit_features = DigitFeatures(z_what_dim)
         self.digit_positions = DigitPositions(z_where_dim)
 
     def forward(self, xs):
+        return self.model(xs)
+
+    def model(self, xs):
         B, T, _, _ = xs.shape
-        z_what = self.digit_features(self._num_digits)
+        z_what = self.digit_features(K=self._num_digits, batch_shape=(B,))
         z_where = None
         for t, x in pyro.markov(enumerate(xs.unbind(1))):
-            z_where = self.digit_positions(t, z_where)
+            z_where = self.digit_positions(t, K=self._num_digits,
+                                           batch_shape=(B,), z_where=z_where)
             self.decoder(t, z_what, z_where, x)
+
+    def guide(self, xs):
+        B, T, _, _ = xs.shape
+
+        data = xs.reshape(xs.shape[0], math.prod(xs.shape[1:]))
+        with asvi(amortizer=self.encoders, data=data, event_shape=xs.shape[1:],
+                  namer=lambda n: n.split('__')[0]):
+            self.digit_features(self._num_digits, batch_shape=(B,))
+
+        z_where = None
+        for t, x in pyro.markov(enumerate(xs.unbind(1))):
+            x = x.reshape(x.shape[0], math.prod(x.shape[1:]))
+            with asvi(amortizer=self.encoders, data=x, event_shape=x.shape[1:],
+                      namer=lambda n: n.split('__')[0]):
+                z_where = self.digit_positions(t, self._num_digits,
+                                               batch_shape=(B,),
+                                               z_where=z_where)
