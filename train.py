2c09ea6fc6d645779e0e99a9b416632e896c585d
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2023-12-05 18:11:19 -0600

    train.py: Pyro compatibility for unsupervised learning
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- train.py
+++ train.py
@@ -1,7 +1,8 @@
 import argparse
 import collections
-import torch
 import numpy as np
+import pyro
+import torch
 import data_loader.data_loaders as module_data
 import model.loss as module_loss
 import model.metric as module_metric
@@ -28,21 +29,13 @@ def main(config):
     model = config.init_obj('arch', module_arch)
     logger.info(model)
 
-    # get function handles of loss and metrics
-    criterion = getattr(module_loss, config['loss'])
-    metrics = [getattr(module_metric, met) for met in config['metrics']]
+    # build optimizer.
+    optimizer = config.init_obj('optimizer', pyro.optim)
 
-    # build optimizer, learning rate scheduler. delete every lines containing lr_scheduler for disabling scheduler
-    trainable_params = filter(lambda p: p.requires_grad, model.parameters())
-    optimizer = config.init_obj('optimizer', torch.optim, trainable_params)
-
-    lr_scheduler = config.init_obj('lr_scheduler', torch.optim.lr_scheduler, optimizer)
-
-    trainer = Trainer(model, criterion, metrics, optimizer,
-                      config=config,
+    trainer = Trainer(model, [], optimizer, config=config,
                       data_loader=data_loader,
                       valid_data_loader=valid_data_loader,
-                      lr_scheduler=lr_scheduler)
+                      lr_scheduler=None)
 
     trainer.train()
 
