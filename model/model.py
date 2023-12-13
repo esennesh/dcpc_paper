38651527319619c705fe48d629be0591a0c1e066
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2023-12-12 20:44:47 -0600

    model.BouncingMnistPpc: bouncing MNIST model with PPC graph tracking
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- model/model.py
+++ model/model.py
@@ -6,7 +6,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from base import BaseModel
-from .inference import asvi, mlp_amortizer
+from .inference import asvi, mlp_amortizer, PpcGraph
 
 class DigitPositions(BaseModel):
     def __init__(self, z_where_dim=2):
@@ -132,3 +132,40 @@ class BouncingMnistAsvi(BaseModel):
                       namer=lambda n: n.split('__')[0]):
                 z_where = self.digit_positions(z_where, t=t, K=self._num_digits,
                                                batch_shape=(B,))
+
+class BouncingMnistPpc(BaseModel):
+    def __init__(self, digit_side=28, hidden_dim=400, num_digits=3, T=10,
+                 temperature=1e-3, x_side=96, z_what_dim=10, z_where_dim=2):
+        super().__init__()
+        self._num_digits = num_digits
+
+        self.decoder = DigitsDecoder(digit_side, hidden_dim, x_side, z_what_dim)
+        self.digit_features = DigitFeatures(z_what_dim)
+        self.digit_positions = DigitPositions(z_where_dim)
+
+        self.graph = PpcGraph(temperature)
+        self.graph.add_node("z_what", [], self.digit_features)
+        for t in range(T):
+            if t == 0:
+                where_kernel = lambda **kwargs: self.digit_positions(None,
+                                                                     **kwargs)
+                self.graph.add_node("z_where__0", [], where_kernel)
+            else:
+                self.graph.add_node("z_where__%d" % t, ["z_where__%d" % (t-1)],
+                                    self.digit_positions)
+            self.graph.add_node("X__%d" % t, ["z_what", "z_where__%d" % t],
+                                self.decoder)
+
+    def forward(self, xs):
+        B, T, _, _ = xs.shape
+        self.graph.set_kwargs("z_what", K=self._num_digits, batch_shape=(B,))
+        z_what = self.digit_features(K=self._num_digits, batch_shape=(B,))
+        z_where = None
+        for t, x in pyro.markov(enumerate(xs.unbind(1))):
+            self.graph.set_kwargs("z_where__%d" % t, t=t, K=self._num_digits,
+                                  batch_shape=(B,))
+            z_where = self.digit_positions(z_where, t=t, K=self._num_digits,
+                                           batch_shape=(B,))
+
+            self.graph.set_kwargs("X__%d" % t, t=t, x=x)
+            self.decoder(z_what, z_where, t=t, x=x)
