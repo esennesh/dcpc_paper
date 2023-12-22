e4146d4017f903fad55d23a36757a24536eca8e7
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2023-12-21 21:05:25 -0600

    data_loaders.BouncingMnistDataLoader: sequential cycling through data-loader
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -50,10 +50,10 @@ class BouncingMnistDataLoader(BaseDataLoader):
     """
     Bouncing MNIST data loading using BaseDataLoader
     """
-    def __init__(self, data_dir, batch_size, shuffle=True, validation_split=0.0, num_workers=1, training=True):
+    def __init__(self, data_dir, batch_size, validation_split=0.0, num_workers=1, training=True):
         trsfm = transforms.Compose([
             transforms.ToTensor(),
         ])
         self.data_dir = data_dir
         self.dataset = BouncingMNIST(self.data_dir, transform=trsfm)
-        super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
+        super().__init__(self.dataset, batch_size, validation_split, num_workers)
