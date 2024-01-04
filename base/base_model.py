a5ab6c43af583e7c3ef09c53f31364a19e28885d
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2024-01-04 14:28:58 -0600

    base_model.MarkovKernel: base class mapping parameters to Pyro distributions
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- base/base_model.py
+++ base/base_model.py
@@ -34,3 +34,34 @@ class BaseModel(pyro.nn.PyroModule):
         resume_path = str(resume_path)
         checkpoint = torch.load(resume_path)
         self.load_state_dict(checkpoint['state_dict'])
+
+class MarkovKernel(pyro.nn.PyroModule):
+    """
+    Base class for Markov kernels that output a Pyro distribution
+    """
+    @abstractmethod
+    def forward(self, *args, **kwargs) -> pyro.distributions.Distribution:
+        """
+        Forward pass logic
+
+        :return: Model output
+        """
+        raise NotImplementedError
+
+    def __str__(self):
+        """
+        Model prints with number of trainable parameters
+        """
+        model_parameters = filter(lambda p: p.requires_grad, self.parameters())
+        params = sum([np.prod(p.size()) for p in model_parameters])
+        return super().__str__() + '\nTrainable parameters: {}'.format(params)
+
+    def resume_from_checkpoint(self, resume_path):
+        """
+        Resume from saved checkpoints
+
+        :param resume_path: Checkpoint path to be resumed
+        """
+        resume_path = str(resume_path)
+        checkpoint = torch.load(resume_path)
+        self.load_state_dict(checkpoint['state_dict'])
