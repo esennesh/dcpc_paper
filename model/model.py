50acfbb2c3fb0f1fae819b006806920f8cbdf4c8
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2024-01-04 14:34:26 -0600

    generative.GraphicalModel: base class for graphical models w/ ancestor sampling
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- model/model.py
+++ model/model.py
@@ -1,91 +1,14 @@
 import math
+import networkx as nx
 import pyro
 import pyro.distributions as dist
 import pyro.nn as pnn
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from base import BaseModel, MarkovKernel
+from base import BaseModel
+from .generative import *
 from .inference import asvi, mlp_amortizer, PpcGraph
-
-class DigitPositions(MarkovKernel):
-    def __init__(self, z_where_dim=2):
-        super().__init__()
-        self.register_buffer('loc', torch.zeros(z_where_dim))
-        self.register_buffer('scale', torch.ones(z_where_dim) * 0.2)
-
-    def forward(self, z_where, K=3, batch_shape=()) -> dist.Distribution:
-        scale = self.scale
-        if z_where is None:
-            scale = scale * 5
-        prior = dist.Normal(self.loc, scale).expand([
-            *batch_shape, K, *self.loc.shape
-        ])
-        return prior.to_event(2)
-
-class DigitFeatures(MarkovKernel):
-    def __init__(self, z_what_dim=10):
-        super().__init__()
-        self.register_buffer('loc', torch.zeros(z_what_dim))
-        self.register_buffer('scale', torch.ones(z_what_dim))
-
-    def forward(self, K=3, batch_shape=()) -> dist.Distribution:
-        prior = dist.Normal(self.loc, self.scale).expand([
-            *batch_shape, K, *self.loc.shape
-        ])
-        return prior.to_event(2)
-
-class DigitsDecoder(MarkovKernel):
-    def __init__(self, digit_side=28, hidden_dim=400, x_side=96, z_what_dim=10):
-        super().__init__()
-        self._digit_side = digit_side
-        self._x_side = x_side
-        self.decoder = nn.Sequential(
-            nn.Linear(z_what_dim, hidden_dim // 2), nn.ReLU(),
-            nn.Linear(hidden_dim // 2, hidden_dim), nn.ReLU(),
-            nn.Linear(hidden_dim, digit_side ** 2), nn.Sigmoid()
-        )
-        scale = torch.diagflat(torch.ones(2) * x_side / digit_side)
-        self.register_buffer('scale', scale)
-        self.translate = (x_side - digit_side) / digit_side
-
-    def blit(self, digits, z_where):
-        P, B, K, _ = z_where.shape
-        affine_p1 = self.scale.repeat(P, B, K, 1, 1)
-        affine_p2 = z_where.unsqueeze(-1) * self.translate
-        affine_p2[:, :, :, 0, :] = -affine_p2[:, :, :, 0, :]
-        grid = F.affine_grid(
-            torch.cat((affine_p1, affine_p2), -1).view(P*B*K, 2, 3),
-            torch.Size((P*B*K, 1, self._x_side, self._x_side)),
-            align_corners=True
-        )
-
-        digits = digits.view(P*B*K, self._digit_side, self._digit_side)
-        frames = F.grid_sample(digits.unsqueeze(1), grid, mode='nearest',
-                               align_corners=True).squeeze(1)
-        return frames.view(P, B, K, self._x_side, self._x_side)
-
-    def forward(self, what, where) -> dist.Distribution:
-        P, B, K, _ = where.shape
-        digits = self.decoder(what)
-        frame = torch.clamp(self.blit(digits, where).sum(-3), 0., 1.)
-        return dist.ContinuousBernoulli(frame).to_event(2)
-
-class DigitDecoder(MarkovKernel):
-    def __init__(self, digit_side=28, hidden_dim=400, z_dim=10):
-        super().__init__()
-        self._digit_side = 28
-        self.decoder = nn.Sequential(
-            nn.Linear(z_dim, hidden_dim // 2), nn.ReLU(),
-            nn.Linear(hidden_dim // 2, hidden_dim), nn.ReLU(),
-            nn.Linear(hidden_dim, digit_side ** 2), nn.Sigmoid()
-        )
-
-    def forward(self, what, x=None) -> dist.Distribution:
-        P, B, _, _ = what.shape
-        estimate = self.decoder(what).view(P, B, 1, self._digit_side,
-                                           self._digit_side)
-        return dist.ContinuousBernoulli(estimate).to_event(3)
 
 class BouncingMnistAsvi(BaseModel):
     def __init__(self, digit_side=28, hidden_dim=400, num_digits=3, T=10,
