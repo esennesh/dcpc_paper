e1e594a406e25e6da16d26d3a9d02a88c00a2eb9
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2024-01-29 10:43:21 -0600

    trainer.PpcTrainer: reweigh gradient of free energy as subsampled over dataset
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -207,7 +207,7 @@ class PpcTrainer(BaseTrainer):
 
         loss = (-log_weight).mean()
         if train:
-            loss.backward()
+            (len(self.data_loader.sampler) / len(data) * loss).backward()
             self.optimizer(pyro.get_param_store().values())
             pyro.infer.util.zero_grads(pyro.get_param_store().values())
 
