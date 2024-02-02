03227aa50549ffb8959743f9b10d8f2b43aeb57b
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2024-02-01 18:13:10 -0600

    model.BouncingMnistPpc: bring back guide() by invoking graph's guide()
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- model/model.py
+++ model/model.py
@@ -126,3 +126,12 @@ class BouncingMnistPpc(BaseModel):
             self.graph.clamp('X__%d' % t, xs[:, t])
         recons = self.graph.forward(**{'X__%d' % t: xs[:, t] for t in range(T)})
         return torch.stack(recons, dim=2)
+
+    def guide(self, xs):
+        B, T, _, _ = xs.shape
+        self.digit_features.batch_shape = (B,)
+        self.digit_positions.batch_shape = (B,)
+        for t in range(T):
+            self.graph.clamp('X__%d' % t, xs[:, t])
+        recons = self.graph.guide(**{'X__%d' % t: xs[:, t] for t in range(T)})
+        return torch.stack(recons, dim=2)
