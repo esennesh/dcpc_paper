esennesh/ppc_experiments

model.MnistPpc: create a PpcGraphicalModel and invoke it in forward(), guide()
Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

model/model.py
@@ -8,7 +8,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from base import BaseModel
 from .generative import *
-from .inference import asvi, mlp_amortizer
+from .inference import PpcGraphicalModel, asvi, mlp_amortizer
 
 class BouncingMnistAsvi(BaseModel):
     def __init__(self, digit_side=28, hidden_dim=400, num_digits=3, T=10,
@@ -77,28 +77,37 @@ class BouncingMnistAsvi(BaseModel):
                 z_where = pyro.sample('z_where__%d' % t, pz_where)
 
 class MnistPpc(BaseModel):
-    def __init__(self, digit_side=28, hidden_dims=[400, 400], z_dims=[10, 128]):
+    def __init__(self, digit_side=28, hidden_dims=[400, 400], z_dims=[10, 128],
+                 temperature=1e-3):
         super().__init__()
         self.prior = GaussianPrior(z_dims[0])
         self.decoder = ConditionalGaussian(hidden_dims[0], z_dims[0], z_dims[1])
         self.likelihood = MlpBernoulliLikelihood(hidden_dims[1], z_dims[1],
                                                  (digit_side, digit_side))
 
-        self.graph = GraphicalModel()
+        self.graph = PpcGraphicalModel(temperature)
         self.graph.add_node("z1", [], self.prior)
         self.graph.add_node("z2", ["z1"], self.decoder)
         self.graph.add_node("X", ["z2"], self.likelihood)
 
     def forward(self, xs=None):
-        B = xs.shape[0] if xs is not None else 1
-        self.graph.clamp("X", xs)
+        if xs is not None:
+            B = xs.shape[0]
+            self.graph.clamp("X", xs)
+        else:
+            B = 1
         self.prior.batch_shape = self.decoder.batch_shape = (B,)
         self.likelihood.batch_shape = (B,)
         return self.graph.forward(X=xs)
 
     def guide(self, xs=None):
-        B = xs.shape[0] if xs is not None else 1
-        return self.graph.guide(batch_shape=(B,), X=xs)
+        if xs is not None:
+            B = xs.shape[0]
+            self.graph.clamp("X", xs)
+        else:
+            B = 1
+        self.digit_features.batch_shape = (B,)
+        return self.graph.guide(X=xs)
 
 class BouncingMnistPpc(BaseModel):
     def __init__(self, digit_side=28, hidden_dim=400, num_digits=3, T=10,

esennesh/dcpc_paper

model/model.py
@@ -8,7 +8,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from base import BaseModel
 from .generative import *
-from .inference import asvi, mlp_amortizer
+from .inference import PpcGraphicalModel, asvi, mlp_amortizer
 
 class BouncingMnistAsvi(BaseModel):
     def __init__(self, digit_side=28, hidden_dim=400, num_digits=3, T=10,
@@ -77,28 +77,40 @@ class BouncingMnistAsvi(BaseModel):
                 z_where = pyro.sample('z_where__%d' % t, pz_where)
 
 class MnistPpc(BaseModel):
-    def __init__(self, digit_side=28, hidden_dims=[400, 400], z_dims=[10, 128]):
+    def __init__(self, digit_side=28, hidden_dims=[400, 400], z_dims=[10, 128],
+                 temperature=1e-3):
         super().__init__()
         self.prior = GaussianPrior(z_dims[0])
         self.decoder = ConditionalGaussian(hidden_dims[0], z_dims[0], z_dims[1])
         self.likelihood = MlpBernoulliLikelihood(hidden_dims[1], z_dims[1],
                                                  (digit_side, digit_side))
 
-        self.graph = GraphicalModel()
+        self.graph = PpcGraphicalModel(temperature)
         self.graph.add_node("z1", [], self.prior)
         self.graph.add_node("z2", ["z1"], self.decoder)
         self.graph.add_node("X", ["z2"], self.likelihood)
 
     def forward(self, xs=None):
-        B = xs.shape[0] if xs is not None else 1
-        self.graph.clamp("X", xs)
+        if xs is not None:
+            B = xs.shape[0]
+            self.graph.clamp("X", xs)
+        else:
+            B = 1
         self.prior.batch_shape = self.decoder.batch_shape = (B,)
         self.likelihood.batch_shape = (B,)
-        return self.graph.forward(X=xs)
+        with clamp_graph(self.graph, X=xs) as graph:
+            return graph.forward()
 
     def guide(self, xs=None):
-        B = xs.shape[0] if xs is not None else 1
-        return self.graph.guide(batch_shape=(B,), X=xs)
+        if xs is not None:
+            B = xs.shape[0]
+            self.graph.clamp("X", xs)
+        else:
+            B = 1
+        self.prior.batch_shape = self.decoder.batch_shape = (B,)
+        self.likelihood.batch_shape = (B,)
+        with clamp_graph(self.graph, X=xs) as graph:
+            return graph.guide()
 
 class BouncingMnistPpc(BaseModel):
     def __init__(self, digit_side=28, hidden_dim=400, num_digits=3, T=10,