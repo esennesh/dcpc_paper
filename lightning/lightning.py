b9a2df5c9f4e0666dac334f1016138c4bd25e53a
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2024-05-06 15:15:21 -0500

    lightning.LightningSvi: Pyro stochastic variational inference with Lightning
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- lightning/lightning.py
+++ lightning/lightning.py
@@ -1,9 +1,10 @@
 import functools
+import lightning as L
 import math
 import numpy as np
 import pyro
 from pyro.optim import lr_scheduler
-from pyro.infer import SVI, JitTraceGraph_ELBO, TraceGraph_ELBO
+from pyro.infer import Importance, Predictive, SVI, JitTraceGraph_ELBO, TraceGraph_ELBO
 import torch
 from torchvision.utils import make_grid
 from base import BaseTrainer
@@ -11,126 +12,56 @@ from model.inference import ParticleDict
 from utils import inf_loop, MetricTracker
 import utils
 
-class Trainer(BaseTrainer):
-    """
-    Trainer class
-    """
-    def __init__(self, model, metric_ftns, optimizer, config, data_loader,
-                 valid_data_loader=None, lr_scheduler=None, len_epoch=None,
-                 num_particles=4, jit=False):
-        super().__init__(model, metric_ftns, optimizer, config)
-        self.config = config
-        self.data_loader = data_loader
-        if len_epoch is None:
-            # epoch-based training
-            self.len_epoch = len(self.data_loader)
-        else:
-            # iteration-based training
-            self.data_loader = inf_loop(data_loader)
-            self.len_epoch = len_epoch
-        self.valid_data_loader = valid_data_loader
-        self.do_validation = self.valid_data_loader is not None
-        self.lr_scheduler = lr_scheduler
-        self.log_step = int(np.sqrt(data_loader.batch_size))
+class LightningSvi(L.LightningModule):
+    def __init__(self, importance, jit=False, lr=1e-3, num_particles=4):
+        super().__init__()
+        self.importance = importance
+        self.lr = lr
         self.num_particles = num_particles
-        self.jit = jit
 
-        self.train_metrics = MetricTracker('loss', *[m.__name__ for m in self.metric_ftns], writer=self.writer)
-        self.valid_metrics = MetricTracker('loss', *[m.__name__ for m in self.metric_ftns], writer=self.writer)
+        if jit:
+            elbo = JitTraceGraph_ELBO(num_particles=self.num_particles,
+                                      max_plate_nesting=1,
+                                      vectorize_particles=True)
+        else:
+            elbo = TraceGraph_ELBO(num_particles=self.num_particles,
+                                   max_plate_nesting=1,
+                                   vectorize_particles=True)
+        self.elbo = elbo(self.importance.model, self.importance.guide)
+        self.predictive = Predictive(self.importance.model,
+                                     guide=self.importance.guide,
+                                     num_samples=self.num_particles)
 
-    def _train_epoch(self, epoch):
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.elbo.parameters(), amsgrad=True,
+                                lr=self.lr, weight_decay=0.)
+
+    def forward(self, *args, **kwargs):
+        return self.predictive(*args, **kwargs)
+
+    def training_step(self, batch, batch_idx):
         """
         Training logic for an epoch
 
-        :param epoch: Integer, current training epoch.
-        :return: A log that contains average loss and metric in this epoch.
+        :param batch: Batch of training data for current training epoch.
+        :return: Loss in this epoch.
         """
-        if self.jit:
-            elbo = JitTraceGraph_ELBO(num_particles=self.num_particles,
-                                      max_plate_nesting=1,
-                                      vectorize_particles=True)
-        else:
-            elbo = TraceGraph_ELBO(num_particles=self.num_particles,
-                                   max_plate_nesting=1,
-                                   vectorize_particles=True)
-        svi = SVI(self.model.model, self.model.guide, self.optimizer, loss=elbo)
+        data, target = batch
+        loss = self.elbo(data)
+        self.log("train_loss", loss)
+        return loss
 
-        self.model.train()
-        self.train_metrics.reset()
-        for batch_idx, (data, target) in enumerate(self.data_loader):
-            data = data.to(self.device)
-            loss = svi.step(data) / data.shape[0]
-
-            self.writer.set_step((epoch - 1) * self.len_epoch + batch_idx)
-            self.train_metrics.update('loss', loss)
-            for met in self.metric_ftns:
-                self.train_metrics.update(met.__name__, met(data))
-
-            if batch_idx % self.log_step == 0:
-                self.logger.debug('Train Epoch: {} {} Loss: {:.6f}'.format(
-                    epoch,
-                    self._progress(batch_idx),
-                    loss))
-                if data.shape[1] == 1:
-                    self.writer.add_image('input', make_grid(data.cpu(), nrow=8, normalize=True))
-
-            if batch_idx == self.len_epoch:
-                break
-        log = self.train_metrics.result()
-
-        if self.do_validation:
-            val_log = self._valid_epoch(epoch)
-            log.update(**{'val_'+k : v for k, v in val_log.items()})
-
-        if self.lr_scheduler is not None:
-            self.lr_scheduler.step()
-        return log
-
-    def _valid_epoch(self, epoch):
+    def validation_step(self, batch, batch_idx):
         """
-        Validate after training an epoch
+        Validation logic for an epoch
 
-        :param epoch: Integer, current training epoch.
-        :return: A log that contains information about validation
+        :param batch: Batch of training data for current validation epoch.
+        :return: Loss in this epoch.
         """
-        if self.jit:
-            elbo = JitTraceGraph_ELBO(num_particles=self.num_particles,
-                                      max_plate_nesting=1,
-                                      vectorize_particles=True)
-        else:
-            elbo = TraceGraph_ELBO(num_particles=self.num_particles,
-                                   max_plate_nesting=1,
-                                   vectorize_particles=True)
-        svi = SVI(self.model.model, self.model.guide, self.optimizer, loss=elbo)
-
-        self.model.eval()
-        self.valid_metrics.reset()
-        with torch.no_grad():
-            for batch_idx, (data, target) in enumerate(self.valid_data_loader):
-                data = data.to(self.device)
-                loss = svi.evaluate_loss(data) / data.shape[0]
-
-                self.writer.set_step((epoch - 1) * len(self.valid_data_loader) + batch_idx, 'valid')
-                self.valid_metrics.update('loss', loss)
-                for met in self.metric_ftns:
-                    self.valid_metrics.update(met.__name__, met(data))
-                if data.shape[1] == 1:
-                    self.writer.add_image('input', make_grid(data.cpu(), nrow=8, normalize=True))
-
-        # add histogram of model parameters to the tensorboard
-        for name, p in self.model.named_parameters():
-            self.writer.add_histogram(name, p, bins='auto')
-        return self.valid_metrics.result()
-
-    def _progress(self, batch_idx):
-        base = '[{}/{} ({:.0f}%)]'
-        if hasattr(self.data_loader, 'n_samples'):
-            current = batch_idx * self.data_loader.batch_size
-            total = self.data_loader.n_samples
-        else:
-            current = batch_idx
-            total = self.len_epoch
-        return base.format(current, total, 100.0 * current / total)
+        data, target = batch
+        loss = self.elbo(data)
+        self.log("train_loss", loss)
+        return loss
 
 class PpcTrainer(BaseTrainer):
     """
