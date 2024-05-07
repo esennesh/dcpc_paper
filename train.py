e1b58d5f3f1416a4e7f967704eff7c8eec30875f
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2024-05-07 17:57:13 -0500

    train: factor out code to construct data, model, trainer from configuration
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- train.py
+++ train.py
@@ -3,57 +3,49 @@ import collections
 import numpy as np
 import pyro
 import torch
-import data_loader.data_loaders as module_data
+import lightning.pytorch
 import model.loss as module_loss
 import model.metric as module_metric
 import model.model as module_arch
 from parse_config import ConfigParser
-import trainer.trainer as module_trainer
+import training
+from utils import read_json
 
 # fix random seeds for reproducibility
 SEED = 123
-torch.manual_seed(SEED)
-torch.backends.cudnn.deterministic = True
-torch.backends.cudnn.benchmark = False
-np.random.seed(SEED)
+lightning.pytorch.seed_everything(SEED, workers=True)
+
+def setup(config):
+    logger = config.init_obj('logger', lightning.pytorch.loggers,
+                             save_dir=config.log_dir)
+
+    # set up data modules
+    data_module = config.init_obj("data_module", training)
+    data_module.prepare_data()
+    data_module.setup(stage="fit")
+
+    # build model architecture and its Lightning module
+    model = config.init_obj("arch", module_arch, data_module.dims)
+    lmodule = config.init_obj("lmodule", training, model, data_module)
+
+    # build Lightning trainer
+    checkpoint = config.init_obj("checkpoint", lightning.pytorch.callbacks,
+                                 dirpath=config.save_dir)
+    trainer = config.init_obj("trainer", lightning.pytorch,
+                              callbacks=[checkpoint], logger=logger,
+                              log_every_n_steps=3)
+    return data_module, lmodule, trainer
+
+def from_file(config_file, checkpoint=None):
+    config = ConfigParser(read_json(config_file), resume=checkpoint)
+    return config, setup(config)
 
 def main(config):
-    logger = config.get_logger('train')
-
-    # setup data_loader instances
-    data_loader = config.init_obj('data_loader', module_data)
-    valid_data_loader = data_loader.split_validation()
-
-    # build model architecture, then print to console
-    model = config.init_obj('arch', module_arch)
-    logger.info(model)
-
-    # get function handles of metrics
-    metrics = [getattr(module_metric, met) for met in config['metrics']]
-
-    # build optimizer.
-    if "lr_scheduler" in config:
-        lr_scheduler = getattr(pyro.optim, config["lr_scheduler"]["type"])
-        lr_scheduler = optimizer = lr_scheduler({
-            "optimizer": getattr(torch.optim, config["optimizer"]["type"]),
-            "optim_args": config["optimizer"]["args"]["optim_args"],
-            **config["lr_scheduler"]["args"]
-        })
-    else:
-        optimizer = config.init_obj('optimizer', pyro.optim)
-        lr_scheduler = None
-
-    # build trainer
-    trainer = config.init_obj('trainer', module_trainer, model, metrics,
-                              optimizer, config=config, data_loader=data_loader,
-                              valid_data_loader=valid_data_loader,
-                              lr_scheduler=lr_scheduler)
-
-    trainer.train()
-
+    dm, model, trainer = setup(config)
+    trainer.fit(model, dm)
 
 if __name__ == '__main__':
-    args = argparse.ArgumentParser(description='PyTorch Template')
+    args = argparse.ArgumentParser(description='Population Predictive Coding (PPC) training script')
     args.add_argument('-c', '--config', default=None, type=str,
                       help='config file path (default: None)')
     args.add_argument('-r', '--resume', default=None, type=str,
@@ -64,8 +56,8 @@ if __name__ == '__main__':
     # custom cli options to modify configuration from default values given in json file.
     CustomArgs = collections.namedtuple('CustomArgs', 'flags type target')
     options = [
-        CustomArgs(['--lr', '--learning_rate'], type=float, target='optimizer;args;lr'),
-        CustomArgs(['--bs', '--batch_size'], type=int, target='data_loader;args;batch_size')
+        CustomArgs(['--lr', '--learning_rate'], type=float, target='lmodule;args;lr'),
+        CustomArgs(['--bs', '--batch_size'], type=int, target='data_module;args;batch_size')
     ]
     config = ConfigParser.from_args(args, options)
     main(config)
