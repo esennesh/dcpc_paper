373e7f0df41e456f838a730b06faee6e0dcb9651
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2024-05-14 02:51:06 +0000

    parse_config.py: remove race condition that hampers training with D>2 devices
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- parse_config.py
+++ parse_config.py
@@ -32,9 +32,8 @@ class ConfigParser:
         self._log_dir = save_dir / 'log' / exper_name / run_id
 
         # make directory for saving checkpoints and log.
-        exist_ok = run_id == ''
-        self.save_dir.mkdir(parents=True, exist_ok=exist_ok)
-        self.log_dir.mkdir(parents=True, exist_ok=exist_ok)
+        self.save_dir.mkdir(parents=True, exist_ok=True)
+        self.log_dir.mkdir(parents=True, exist_ok=True)
 
         # save updated config file to the checkpoint dir
         write_json(self.config, self.save_dir / 'config.json')
