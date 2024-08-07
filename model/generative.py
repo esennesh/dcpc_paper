dd49410d4d7da2b729e9e2e4eb09ec1ef75bf065
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2024-08-07 14:19:38 -0400

    model.generative.ConvolutionalDecoder: add back optimally learned log-scale
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- model/generative.py
+++ model/generative.py
@@ -354,6 +354,7 @@ class ConvolutionalDecoder(MarkovKernel):
             nn.ConvTranspose2d(32, channels, 4, 2, 1),
             nonlinearity()
         )
+        self.log_scale = nn.Parameter(torch.zeros(()))
 
     @property
     def event_dim(self):
@@ -365,9 +366,10 @@ class ConvolutionalDecoder(MarkovKernel):
         hs = hs.view(P*B, self._hidden_dim, 1, 1)
         hs = self.convs(hs).view(P, B, self._channels, self._img_side,
                                  self._img_side)
+        scale = self.log_scale.exp() + 1e-6
         if self._discretize:
-            return DiscretizedGaussian(hs, 1e-2).to_event(3)
-        return dist.Normal(hs, 1e-2).to_event(3)
+            return DiscretizedGaussian(hs, scale).to_event(3)
+        return dist.Normal(hs, scale).to_event(3)
 
 class FixedVarianceDecoder(MarkovKernel):
     def __init__(self, channels=3, img_side=64, scale=0.01, z_dim=64):
