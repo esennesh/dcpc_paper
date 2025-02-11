49180d8c893fa47e4fabe065b0b75d52e1b13e89
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2025-02-10 21:32:48 -0600

    Revert "training.lightning.LightningDcpc: cosine annealing LR schedule"
    This reverts commit ce72f46016d8a5bd42ef5962415d1754ca1b9a30.

--- training/lightning.py
+++ training/lightning.py
@@ -210,8 +210,9 @@ class LightningDcpc(L.LightningModule):
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.graph.parameters(), amsgrad=True,
                                      lr=self.lr, weight_decay=0.)
-        lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
-            optimizer, self.patience, eta_min=1e-6,
+        lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+            optimizer, cooldown=self.cooldown, factor=self.factor,
+            patience=self.patience
         )
         return {"lr_scheduler": lr_scheduler, "monitor": "valid/loss",
                 "optimizer": optimizer}
