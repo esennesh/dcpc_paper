8b858530c8728a8fd026d09efc933a881fc10003
Eli Sennesh <eli.sennesh@vanderbilt.edu>
2025-05-07 10:46:06 -0500

    model.generative.DigitsDecoder: simplify SiLUs into LeakyReLUs
    Signed-off-by: Eli Sennesh <eli.sennesh@vanderbilt.edu>

--- model/generative.py
+++ model/generative.py
@@ -65,9 +65,9 @@ class DigitsDecoder(MarkovKernel):
         self._x_side = x_side
         self.decoder = nn.Sequential(
             nn.Linear(z_what_dim, hidden_dim // 2),
-            nn.LayerNorm((hidden_dim // 2,)), nn.SiLU(),
+            nn.LayerNorm((hidden_dim // 2,)), nn.LeakyReLU(),
             nn.Linear(hidden_dim // 2, hidden_dim),
-            nn.LayerNorm((hidden_dim,)), nn.SiLU(),
+            nn.LayerNorm((hidden_dim,)), nn.LeakyReLU(),
             nn.Linear(hidden_dim, digit_side ** 2), nn.Sigmoid()
         )
         self.register_buffer('scale', torch.eye(2) * x_side / digit_side)
